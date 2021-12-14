3f32acbd7b100b874f236473494c1c387abf708b
mitsutoshi yoshida <s.d.r.economy@gmail.com>
2021-12-14 16:33:49 +0900

    Undo

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,25 +2,21 @@ CarrierWave.configure do |config|
   require 'carrierwave/storage/abstract'
   require 'carrierwave/storage/file'
   require 'carrierwave/storage/fog'
-
   config.storage :fog
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     Rails.application.credentials.aws[:aws_access_key_id],
-      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
       region:                'ap-northeast-1',
       path_style:            true,
   }
-#  aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
-#  aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-
   config.fog_public     = true
   config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
   case Rails.env
     when 'production'
-      config.fog_directory = 'aws-image-app'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/aws-image-app'
+      config.fog_directory = 'aws1-production'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/aws1-production'
     when 'development'
       config.fog_directory = 'aws1-development'
       config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/aws1-development'
