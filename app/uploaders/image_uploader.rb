320559128cfeff67f5ceba3c6f49155adbeeedcc
mitsutoshi yoshida <s.d.r.economy@gmail.com>
2021-12-14 14:21:28 +0900

    image_uploader updated

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,11 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  storage :fog
+  if Rails.env.development?
+    storage :file
+  else
+    storage :fog
+  end
   process resize_to_fill: [300, 300, "Center"]
 
   # Override the directory where uploaded files will be stored.
